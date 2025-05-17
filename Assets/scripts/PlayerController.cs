11c22f2ca1e8d0f2c669ccfaf15e4fea9d136189
Andriy Babiy <andriybabiy97@gmail.com>
2025-05-17 17:53:26 +0100

    adjustments to player sprite
    - change skin
    - add hit radius
    - add hit radius adjustment on movement
    - added damage to enemy sprite

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,10 +1,16 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed;
+    public Vector3 playerMoveDirection;
+    public int damage_val;
+    public float damage_rad_offset_x;
+    public float damage_rad_offset_y;
+    [SerializeField] private float inputX;
+    [SerializeField] private float inputY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,9 +20,45 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+        playerMoveDirection = new Vector3(inputX, inputY).normalized;
 
-        rb.linearVelocity = new Vector3(inputX, inputY).normalized;
+        animator.SetFloat("moveX", inputX);
+        animator.SetFloat("moveY", inputY);
+
+        if (playerMoveDirection ==  Vector3.zero )
+        {
+            animator.SetBool("moving", false);
+        }
+        else
+        {
+            animator.SetBool("moving", true);
+            
+        }
+
+        // down -> (0, 0) = (0, -1)
+        // up -> (0, 0.5) = (0, 1)
+        // left -> (-0.25, 0.25) = (-1, 0)
+        // left -> (0.25, 0.25) = (1, 0)
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var a = Physics2D.OverlapCircleAll(transform.position + new Vector3(inputX * damage_rad_offset_x, inputY * damage_rad_offset_y + damage_rad_offset_y + 0.5f), 0.5f);
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i].GetComponent<Enemy>().TakeDamage(damage_val);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position + new Vector3(inputX * damage_rad_offset_y, inputY * damage_rad_offset_x + damage_rad_offset_y + 0.5f), 0.5f);
+    }
+    private void FixedUpdate()
+    {
+            rb.linearVelocity = new Vector3(playerMoveDirection.x, playerMoveDirection.y).normalized * moveSpeed;
     }
 }
